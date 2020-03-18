8a61c7110b0eb60ef7fcd83ddfa475be3349fcbc
muyuanqiang <muyuanqiang7@gmail.com>
2020-03-18 09:41:14 +0800

    feat(cache): add redis cache config
    add redis cache config
    add sentry monitor paltform
    add logback config
    file

--- src/main/java/com/huiztech/muyq/spring/boot/sentry/controller/SentryController.java
+++ src/main/java/com/huiztech/muyq/spring/boot/sentry/controller/SentryController.java
@@ -1,11 +1,11 @@
 package com.huiztech.muyq.spring.boot.sentry.controller;
 
+import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -21,12 +21,25 @@ public class SentryController {
 
     @GetMapping
     public Map<String, Object> createSentryEvent() {
-        Map<String, Object> data = new HashMap<>();
+        Map<String, Object> data = Maps.newHashMap();
         try {
             throw new RuntimeException("sentry catch and report runtime error");
         } catch (RuntimeException e) {
             log.error("sentry event error测试信息", e);
         }
+        data.put("code", "10001");
+        data.put("data", "debug");
+        data.put("status", false);
+        return data;
+    }
+
+    @GetMapping(value = "greeting")
+    public Map<String, Object> greeting() {
+        Map<String, Object> data = Maps.newHashMap();
+        log.info("greeting from: 127.0.0.1");
+        data.put("code", "10001");
+        data.put("data", "debug");
+        data.put("status", false);
         return data;
     }
 }
--- src/test/java/com/huiztech/muyq/spring/boot/sentry/SpringBootSentryApplicationTests.java
+++ src/test/java/com/huiztech/muyq/spring/boot/sentry/SpringBootSentryApplicationTests.java
@@ -1,13 +1,25 @@
 package com.huiztech.muyq.spring.boot.sentry;
 
+import com.google.common.collect.Maps;
+import com.huiztech.muyq.spring.boot.sentry.cache.RedisService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.Map;
 
 @SpringBootTest
 class SpringBootSentryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Resource
+    private RedisService redisService;
+
+    @Test
+    void contextLoads() {
+        Map<String, Object> data = Maps.newHashMap();
+        data.put("user", "entry");
+        data.put("info", "came from lost city");
+        redisService.set("sentry:mall", data);
+    }
 
 }
